194b7c5a6bf8867c17d42f473d1fb1d2c77352d1
Natsuki0117 <natsu000117@icloud.com>
2024-08-28 20:45:52 +0900

    firestore

--- ToDoTask/FirestoreClient.swift
+++ ToDoTask/FirestoreClient.swift
@@ -14,30 +14,30 @@ class FirestoreClient {
     static var tasks: CollectionReference = Firestore.firestore().collection("tasks")
     
     //全部async awaitで！！！　↓こういう書き方
-//    static func aaaa() {
-//        Task {
-//            do {
-//                try await ....
-//            } catch {
-//                print(error.localizedDescription)
-//            }
-//        }
-//    }
+    //    static func aaaa() {
+    //        Task {
+    //            do {
+    //                try await ....
+    //            } catch {
+    //                print(error.localizedDescription)
+    //            }
+    //        }
+    //    }
     
-//    static func fetchTags() async throws -> [Tag] {
-//        guard FirebaseAuthManager.shared.user != nil else { return [] }
-//        return try await tags.getDocuments().documents.compactMap { try? $0.data(as: Tag.self) }
-//    }
+    //    static func fetchTags() async throws -> [Tag] {
+    //        guard FirebaseAuthManager.shared.user != nil else { return [] }
+    //        return try await tags.getDocuments().documents.compactMap { try? $0.data(as: Tag.self) }
+    //    }
     
     static func fetchAll() async -> [TaskItem] {
         guard let userId = Auth.auth().currentUser?.uid else{ return[]}
         do {
             //ユーザーのuserIdでフィルターする
-//            tasks.whereField("userId", isEqualTo: /*userId*/).getDocuments()
             return try await tasks.whereField("userId", isEqualTo: userId).getDocuments().documents.compactMap { try? $0.data(as: TaskItem.self) }
-           
+            
         } catch {
             print("aaa")
+            return []
         }
         
     }
@@ -50,17 +50,17 @@ class FirestoreClient {
                 print(error.localizedDescription)
             }
         }
-     
+        
     }
     
-    static func update(task: TaskItem) {
-        tasks.document(...).set
-//        tasks.document("id").setData(..)
-    }
-    
-    static func delete(task: TaskItem) {
-        tasks....
-//        tasks.document("id").delete()
-    }
+    //    static func update(task: TaskItem) {
+    //        tasks.document(...).set
+    ////        tasks.document("id").setData(..)
+    //    }
+    //
+    //    static func delete(task: TaskItem) {
+    //        tasks....
+    ////        tasks.document("id").delete()
+    //    }
     
 }
