29cdc010d6e1001ecf3728433b8d77cc63691633
Natsuki0117 <natsu000117@icloud.com>
2024-09-09 23:17:15 +0900

    package落ちた

---
+++ ToDoTask/LogInView1.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView1.swift
+//  ToDoTask
+//
+//  Created by 金井菜津希 on 2024/09/09.
+//
+
+import SwiftUI
+
+struct LogInView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInView1()
+}
