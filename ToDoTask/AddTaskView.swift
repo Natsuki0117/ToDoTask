b977978ff27b08f91920e5fe3d84d422f6873900
Natsuki0117 <natsu000117@icloud.com>
2024-09-09 23:18:07 +0900

    データとってくるとこできてない

---
+++ ToDoTask/AddTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskView.swift
+//  ToDoTask
+//
+//  Created by 金井菜津希 on 2024/09/09.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
